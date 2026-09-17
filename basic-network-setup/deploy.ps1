7ceb59c7eb6789dd46c08aa8c9d0a33b71d48023
mcnizzle02 <lee.mcnutt02@gmail.com>
2026-09-16 20:42:36 -0500

    "subnet and NSG's created"

--- basic-network-setup/deploy.ps1
+++ basic-network-setup/deploy.ps1
@@ -23,3 +23,34 @@ az network vnet create `
     --subnet-name $env:FRONTEND_SUBNET_NAME `
     --subnet-prefixes "10.0.1.0/24" `
     --tags purpose=recipe environment=demo
+
+
+ az network vnet subnet create `
+    --resource-group $env:RESOURCE_GROUP `
+    --vnet-name $env:VNET_NAME `
+    --name $env:BACKEND_SUBNET_NAME `
+    --address-prefixes "10.0.2.0/24"
+
+az network vnet subnet create `
+    --resource-group $env:RESOURCE_GROUP `
+    --vnet-name $env:VNET_NAME `
+    --name $env:DATABASE_SUBNET_NAME `
+    --address-prefixes "10.0.3.0/24"
+
+
+az network nsg create `
+    --resource-group $env:RESOURCE_GROUP `
+    --name "nsg-$($env:FRONTEND_SUBNET_NAME)" `
+    --location $env:LOCATION `
+    --tags purpose=recipe environment=demo tier=frontend
+
+az network nsg rule create `
+    --resource-group $env:RESOURCE_GROUP `
+    --nsg-name "nsg-$($env:FRONTEND_SUBNET_NAME)" `
+    --name "Allow-HTTP" `
+    --protocol tcp `
+    --priority 1000 `
+    --destination-port-ranges 80 `
+    --access allow `
+    --direction inbound `
+    --source-address-prefixes "*"
